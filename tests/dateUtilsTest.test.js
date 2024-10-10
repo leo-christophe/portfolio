80e359fa265df8d297feea9a7363d4be74b46206
leo-christophe <leo.christophe@etu.univ-savoie.fr>
2024-10-10 20:22:05 +0200

    Améliorations de certaines traductions et ajout de tests pour la classe dateUtils.

---
+++ tests/dateUtilsTest.test.js
@@ -0,0 +1,65 @@
+import { describe, it, expect } from 'vitest';
+import DateUtils from '../src/utils/date_utils';
+
+describe('DateUtils', () => {
+  
+  // Test de formatDateRange
+  describe('formatDateRange', () => {
+    it('devrait formater correctement une plage de dates', () => {
+      const result = DateUtils.formatDateRange('2023-06-01', '2023-06-10');
+      expect(result).toBe('01 juin 2023 - 10 juin 2023');
+    });
+
+    it('devrait retourner la date seule si les dates de début et de fin sont identiques', () => {
+      const result = DateUtils.formatDateRange('2023-06-01', '2023-06-01');
+      expect(result).toBe('01 juin 2023');
+    });
+
+    it('devrait lancer une erreur si la date de début est null', () => {
+      expect(() => DateUtils.formatDateRange(null, '2023-06-10')).toThrow('La date de début ou la date de fin est null');
+    });
+
+    it('devrait retourner la date de début si la date de fin est null', () => {
+      const result = DateUtils.formatDateRange('2023-06-01', null);
+      expect(result).toBe('01 juin 2023');
+    });
+    
+
+    it('devrait lancer une erreur si la date de début est supérieure à la date de fin', () => {
+      expect(() => DateUtils.formatDateRange('2023-06-10', '2023-06-01')).toThrow('La date de début est supérieure à la date de fin');
+    });
+
+    it('devrait formater correctement une plage de dates dans une locale différente', () => {
+      const result = DateUtils.formatDateRange('2023-06-01', '2023-06-10', 'en-US');
+      expect(result).toBe('June 01, 2023 - June 10, 2023');
+    });
+  });
+
+  // Test de formatDate
+  describe('formatDate', () => {
+    it('devrait formater une date correctement', () => {
+      const result = DateUtils.formatDate('2023-06-01');
+      expect(result).toBe('01 juin 2023');
+    });
+
+    it('devrait formater une date dans une locale différente', () => {
+      const result = DateUtils.formatDate('2023-06-01', 'en-US');
+      expect(result).toBe('June 01, 2023');
+    });
+  });
+
+  // Test de isPastDate
+  describe('isPastDate', () => {
+    it('devrait retourner true si la date est passée', () => {
+      const result = DateUtils.isPastDate('2020-01-01');
+      expect(result).toBe(true);
+    });
+
+    it('devrait retourner false si la date est dans le futur', () => {
+      const futureDate = new Date();
+      futureDate.setFullYear(futureDate.getFullYear() + 1);
+      const result = DateUtils.isPastDate(futureDate.toISOString().split('T')[0]);
+      expect(result).toBe(false);
+    });
+  });
+});
